a2e4389780c4ec2b37057936af2507d597edd640
myrosandrade89 <67491368+myrosandrade89@users.noreply.github.com>
2021-01-20 18:07:25 -0600

    areas estudio screen terminada

--- lib/utilities/components/fonts.dart
+++ lib/utilities/components/fonts.dart
@@ -30,11 +30,13 @@ class fontStyleAmaranth extends StatelessWidget {
       {@required this.text,
       @required this.sizePercentage,
       @required this.color,
-      this.letterSpacing});
+      this.letterSpacing,
+      this.textAlign});
   final String text;
   final double sizePercentage;
   final Color color;
   final double letterSpacing;
+  final TextAlign textAlign;
 
   @override
   Widget build(BuildContext context) {
@@ -45,6 +47,7 @@ class fontStyleAmaranth extends StatelessWidget {
           fontSize: ResponsiveFlutter.of(context).fontSize(sizePercentage),
           fontWeight: FontWeight.bold,
           letterSpacing: letterSpacing != null ? letterSpacing : 0),
+      textAlign: textAlign != null ? textAlign : TextAlign.center,
     );
   }
 }
